094e0f2e88cbf1bbdd477e9e663ef10339476840
Gabriel Teodoro <gabriel.skywalkerr@gmail.com>
2019-11-16 19:30:45 -0300

    fix connection

--- classes/Conexao.php
+++ classes/Conexao.php
@@ -2,15 +2,35 @@
 
 class Conexao {
     public static function pegarConexao() {
+        
+    define("HOST", "localhost");
+    define("DBNAME", "bdlojacarros");
+    define("USERNM", "root");
+    define("PASSWD", "299792458");
+
+    $conexao = new PDO("mysql:host=" . HOST . ";dbname=" . DBNAME, USERNM, PASSWD);
+        
+        
+        
+        
+        
+        // Login G4L1LE0
+        
         // Login tabata
-        // $conexao = new PDO("mysql:host=localhost;
-        // dbname=bdlojacarros", "admin", "Msql_20191"); 
+        /*
+        $conexao = new PDO("mysql:host=localhost;
+        dbname=bdlojacarros", "admin", "Msql_20191");
+        */
         //Login miguel
+        /*
         $conexao = new PDO(
             "mysql:host=localhost;
             dbname=bdlojacarros",
             "root",
-            "");  
+            "");
+        */  
+
+        
         $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $conexao->exec("SET CHARACTER SET utf8");
         return $conexao;
