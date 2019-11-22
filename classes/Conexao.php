609985a23630e7338669a113636426e63c460581
t4b4t4F33h <tabatafssilva@gmail.com>
2019-11-22 02:53:08 -0200

    voltando o conexão pra como era antes, sem constante, sem bug

--- classes/Conexao.php
+++ classes/Conexao.php
@@ -3,12 +3,13 @@
 class Conexao {
     public static function pegarConexao() {
     
-        define("HOST", "localhost");
-        define("DBNAME", "bdlojacarros");
-        define("USERNM", "admin");
-        define("PASSWD", "Msql_20191");
+        // define("HOST", "localhost");
+        // define("DBNAME", "bdlojacarros");
+        // define("USERNM", "admin");
+        // define("PASSWD", "Msql_20191");
 
-        $conexao = new PDO("mysql:host=" . HOST . ";dbname=" . DBNAME, USERNM, PASSWD);    
+        $conexao = new PDO("mysql:host=localhost;
+        dbname=bdlojacarros", "admin", "Msql_20191"); 
         $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $conexao->exec("SET CHARACTER SET utf8");
         return $conexao;
